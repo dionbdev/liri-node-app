9911d5a5389bf1868c059e7355e15c7cd4123c23
d bailey <d bailey>
2016-07-16 00:14:31 -0400

    node app

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '<A62OBhpX2J3z9kwLrwUb6j4kT>',
+  consumer_secret: '<FdSvTna9VYAr1fixTyYl7CHwU26EnOWey5eA4HKfeWUV0ErH1l>',
+  access_token_key: '<1970164826-TbzKszhtaOcQoSRKh5gT3Y9A5uWf0aR29sDOCq2>',
+  access_token_secret: '<moaXlYKIgUF06eB6C2UVcqbdccGdulPlT65WpJY8NeUio>',
+}
